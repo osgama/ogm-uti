4d1ec1ac51105366becddc42c722d38e2e95ba27
OSGAMA <osgama94@gmail.com>
2023-11-07 02:29:29 -0600

    shelllll

--- Batch.bat
+++ Batch.bat
@@ -102,3 +102,13 @@ ECHO * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 ECHO *                 Saliendo, ¡Adios!                   *
 ECHO * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 EXIT /b
+
+
+    0 = Negro       8 = Gris
+    1 = Azul        9 = Azul claro
+    2 = Verde       A = Verde claro
+    3 = Aguamarina  B = Aguamarina claro
+    4 = Rojo        C = Rojo claro
+    5 = Púrpura     D = Púrpura claro
+    6 = Amarillo    E = Amarillo claro
+    7 = Blanco      F = Blanco brillante
