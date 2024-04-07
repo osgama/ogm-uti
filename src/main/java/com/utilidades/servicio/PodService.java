7ad56c8832e53ba3307f68a54db6fde66cde33a5
osgama <134085039+osgama@users.noreply.github.com>
2024-04-07 15:08:13 -0600

    mensajes de log

--- src/main/java/com/utilidades/servicio/PodService.java
+++ src/main/java/com/utilidades/servicio/PodService.java
@@ -18,58 +18,93 @@ public class PodService {
     private List<String> listaDePods = Arrays.asList("podName1", "podName2", "podName3", "podName16");
     private static final String NAMESPACE = "tu-namespace";
 
-    private OpenShiftClient createOpenShiftClient(String token, String servidor) {
-        Config config = new ConfigBuilder()
-                .withPassword(token)
-                .withMasterUrl(servidor)
-                .build();
 
-        KubernetesClient kubernetesClient = new KubernetesClientBuilder().withConfig(config).build();
-        return kubernetesClient.adapt(OpenShiftClient.class);
+    private OpenShiftClient createOpenShiftClient(String token, String servidor) {
+        System.out.println("Intentando crear cliente de OpenShift...");
+
+        try {
+            Config config = new ConfigBuilder()
+                    .withOauthToken(token) // Se recomienda usar withOauthToken para tokens de acceso
+                    .withMasterUrl(servidor)
+                    .withTrustCerts(true) // Considera las implicaciones de seguridad de esta opción
+                    .build();
+
+            KubernetesClient kubernetesClient = new KubernetesClientBuilder().withConfig(config).build();
+            kubernetesClient.namespaces().list();
+
+            System.out.println("Cliente de OpenShift creado y validado exitosamente.");
+            return kubernetesClient.adapt(OpenShiftClient.class);
+        } catch (Exception e) {
+            System.err.println("Error al crear el cliente de OpenShift: " + e.getMessage());
+            throw new RuntimeException("Error al establecer conexión con OpenShift: " + e.getMessage(), e);
+        }
     }
 
     public void scaleDownPods(String token, String servidor) throws Exception {
+        System.out.println("Iniciando el escalado hacia abajo de los pods...");
         try (OpenShiftClient openShiftClient = createOpenShiftClient(token, servidor)) {
             listaDePods.forEach(podName -> {
-                DeploymentConfig dc = openShiftClient.deploymentConfigs().inNamespace(NAMESPACE).withName(podName)
-                        .get();
-                if (dc != null && dc.getSpec() != null) {
-                    openShiftClient.deploymentConfigs().inNamespace(NAMESPACE).withName(podName).scale(0);
+                try {
+                    DeploymentConfig dc = openShiftClient.deploymentConfigs().inNamespace(NAMESPACE).withName(podName).get();
+                    if (dc != null && dc.getSpec() != null) {
+                        openShiftClient.deploymentConfigs().inNamespace(NAMESPACE).withName(podName).scale(0);
+                        System.out.println("Pod " + podName + " escalado a 0 exitosamente.");
+                    }
+                } catch (Exception e) {
+                    System.err.println("Error al escalar el pod " + podName + ": " + e.getMessage());
                 }
             });
+        } catch (Exception e) {
+            System.err.println("Error al intentar escalar los pods hacia abajo: " + e.getMessage());
         }
+        System.out.println("Escalado hacia abajo de pods completado.");
     }
 
     public void scaleUpPodsInBlocks(String token, String servidor) throws Exception {
+        System.out.println("Iniciando el escalado hacia arriba de los pods en bloques...");
         try (OpenShiftClient openShiftClient = createOpenShiftClient(token, servidor)) {
             for (int i = 0; i < listaDePods.size(); i += BLOCK_SIZE) {
                 List<String> currentBlock = listaDePods.subList(i, Math.min(i + BLOCK_SIZE, listaDePods.size()));
+                System.out.println("Escalando el siguiente bloque de pods: " + currentBlock);
                 currentBlock.forEach(podName -> {
-                    DeploymentConfig dc = openShiftClient.deploymentConfigs().inNamespace(NAMESPACE).withName(podName)
-                            .get();
-                    if (dc != null && dc.getSpec() != null) {
-                        openShiftClient.deploymentConfigs().inNamespace(NAMESPACE).withName(podName).scale(1);
+                    try {
+                        DeploymentConfig dc = openShiftClient.deploymentConfigs().inNamespace(NAMESPACE).withName(podName).get();
+                        if (dc != null && dc.getSpec() != null) {
+                            openShiftClient.deploymentConfigs().inNamespace(NAMESPACE).withName(podName).scale(1);
+                            System.out.println("Pod " + podName + " escalado a 1 exitosamente.");
+                        }
+                    } catch (Exception e) {
+                        System.err.println("Error al escalar el pod " + podName + ": " + e.getMessage());
                     }
                 });
 
-                // Espera a que todos los pods estén listos antes de continuar
                 boolean allReady;
                 do {
                     allReady = checkPodsReady(openShiftClient, currentBlock);
-                    Thread.sleep(10000); // Espera 10 segundos antes de volver a verificar
+                    if (!allReady) {
+                        System.out.println("Esperando a que todos los pods del bloque actual estén listos...");
+                        Thread.sleep(10000); // Espera 10 segundos antes de volver a verificar
+                    }
                 } while (!allReady);
-                // Espera 2 minutos antes de avanzar al siguiente bloque de pods
-                System.out.println(
-                        "Todos los pods del bloque actual están listos. Esperando 2 minutos antes de continuar con el siguiente bloque.");
+
+                System.out.println("Todos los pods del bloque actual están listos. Esperando 2 minutos antes de continuar con el siguiente bloque.");
                 Thread.sleep(120000); // 120000 milisegundos = 2 minutos
             }
+        } catch (Exception e) {
+            System.err.println("Error durante el escalado hacia arriba de los pods: " + e.getMessage());
         }
+        System.out.println("Escalado hacia arriba de pods en bloques completado.");
     }
 
     private boolean checkPodsReady(OpenShiftClient openShiftClient, List<String> podNames) {
         return podNames.stream()
-                .allMatch(podName -> openShiftClient.pods().inNamespace(NAMESPACE).withLabel("name", podName).list()
-                        .getItems().stream()
-                        .allMatch(pod -> "Running".equals(pod.getStatus().getPhase())));
+                .allMatch(podName -> {
+                    try {
+                        return openShiftClient.pods().inNamespace(NAMESPACE).withLabel("name", podName).list().getItems().stream().allMatch(pod -> "Running".equals(pod.getStatus().getPhase()));
+                    } catch (Exception e) {
+                        System.err.println("Error al verificar si los pods están listos: " + e.getMessage());
+                        return false;
+                    }
+                });
     }
 }
