a9f106b5ac7b3a71908c1f34a4daafb2d33afc4e
osgama <134085039+osgama@users.noreply.github.com>
2024-04-09 03:05:29 -0600

    emitter
    emitter

--- src/main/java/com/utilidades/api/PodController.java
+++ src/main/java/com/utilidades/api/PodController.java
@@ -4,15 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.utilidades.servicio.PodService;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import java.io.IOException;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
-@Tag(name = "Pod Management", description = "Operaciones para gestionar Pods")
 @RestController
 public class PodController {
 
@@ -22,40 +16,43 @@ public class PodController {
         this.podService = podService;
     }
 
-    @GetMapping("/StopPods")
-    @Operation(summary = "Detiene una serie de pods",
-    description = "Detiene los pods especificados según los parámetros proporcionados.",
-    responses = {
-        @ApiResponse(responseCode = "200",description = "Deteniendo pods, en progreso...", 
-                     content = @Content(schema = @Schema(implementation = String.class)))
-    })
-    public String scaleDownPods(@Parameter(description = "Token de autenticación") @RequestParam String token,
-            @Parameter(description = "Nombre del servidor") @RequestParam String servidor,
-            @Parameter(description = "Opción de selección de pods") @RequestParam String opcion) {
-        try {
-            podService.scaleDownPods(token, servidor, opcion);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "Deteniendo pods, en progreso...";
+    @GetMapping("/streamScaleDownPods")
+    public SseEmitter scaleDownPods(@RequestParam String token, @RequestParam String servidor, @RequestParam String opcion) {
+        final SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+        Thread thread = new Thread(() -> {
+            try {
+                podService.scaleDownPods(token, servidor, opcion, emitter);
+                emitter.complete();
+            } catch (Exception e) {
+                try {
+                    emitter.send(SseEmitter.event().name("error").data("Error al detener los pods" + e.getMessage()));
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                emitter.completeWithError(e);
+            }
+        });
+        thread.start();
+        return emitter;
     }
 
-    @GetMapping("/StartPods")
-    @Operation(summary = "Inicia una serie de pods",
-    description = "Inicia los pods especificados según los parámetros proporcionados.",
-    responses = {
-        @ApiResponse(responseCode = "200",description = "Inicia pods, en progreso...", 
-                     content = @Content(schema = @Schema(implementation = String.class)))
-    })
-    public String scaleUpPodsInBlocks(@Parameter(description = "Token de autenticación") @RequestParam String token,
-            @Parameter(description = "Nombre del servidor") @RequestParam String servidor,
-            @Parameter(description = "Opción de selección de pods") @RequestParam String opcion) {
-
-        try {
-            podService.scaleUpPodsInBlocks(token, servidor, opcion);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "Iniciando pods, en progreso...";
+    @GetMapping("/streamScaleUpPods")
+    public SseEmitter scaleUpPodsInBlocks(@RequestParam String token, @RequestParam String servidor, @RequestParam String opcion) {
+        final SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+        Thread thread = new Thread(() -> {
+            try {
+                podService.scaleUpPodsInBlocks(token, servidor, opcion, emitter);
+                emitter.complete();
+            } catch (Exception e) {
+                try {
+                    emitter.send(SseEmitter.event().name("error").data("Error al iniciar los pods: " + e.getMessage()));
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                emitter.completeWithError(e);
+            }
+        });
+        thread.start();
+        return emitter;
     }
 }
--- src/main/java/com/utilidades/servicio/PodService.java
+++ src/main/java/com/utilidades/servicio/PodService.java
@@ -5,133 +5,126 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.openshift.api.model.DeploymentConfig;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
-
 import io.fabric8.kubernetes.client.Config;
 import io.fabric8.kubernetes.client.ConfigBuilder;
-
 @Service
 public class PodService {
 
     private static final int BLOCK_SIZE = 4;
-
     private List<String> listaDePodsA = Arrays.asList("podName1", "podName2", "podName3", "podName16");
     private List<String> listaDePodsB = Arrays.asList("podName5", "podName6", "podName7", "podName8");
-
     private static final String NAMESPACE = "tu-namespace";
 
-    private OpenShiftClient createOpenShiftClient(String token, String servidor) {
-        System.out.println("Intentando crear cliente de OpenShift...");
-
+    private OpenShiftClient createOpenShiftClient(String token, String servidor, SseEmitter emitter) throws IOException {
         try {
+            emitter.send(SseEmitter.event().name("message").data("Intentando crear cliente de OpenShift..."));
             Config config = new ConfigBuilder()
-                        .withOauthToken(token)
-                        .withMasterUrl(servidor)
-                        .withTrustCerts(true)
-                        .build();
-
+                    .withOauthToken(token)
+                    .withMasterUrl(servidor)
+                    .withTrustCerts(true)
+                    .build();
             KubernetesClient kubernetesClient = new KubernetesClientBuilder().withConfig(config).build();
             kubernetesClient.namespaces().list();
-
-            System.out.println("Cliente de OpenShift creado y validado exitosamente.");
+            emitter.send(SseEmitter.event().name("message").data("Cliente de OpenShift creado y validado exitosamente."));
             return kubernetesClient.adapt(OpenShiftClient.class);
         } catch (Exception e) {
-            System.err.println("Error al crear el cliente de OpenShift: " + e.getMessage());
+            emitter.send(SseEmitter.event().name("error").data("Error al crear el cliente de OpenShift: " + e.getMessage()));
             throw new RuntimeException("Error al establecer conexión con OpenShift: " + e.getMessage(), e);
         }
     }
 
-    public void scaleDownPods(String token, String servidor, String opcion) throws Exception {
-        System.out.println("Iniciando el escalado hacia abajo de los pods...");
-        List<String> listaDePodsDown = seleccionarListaPods(opcion);
-
-        try (OpenShiftClient openShiftClient = createOpenShiftClient(token, servidor)) {
-            escalarPods(openShiftClient, listaDePodsDown, 0);
+    public void scaleDownPods(String token, String servidor, String opcion, SseEmitter emitter) throws IOException {
+        emitter.send(SseEmitter.event().name("message").data("Iniciando el escalado hacia abajo de los pods..."));
+        List<String> listaDePodsDown = seleccionarListaPods(opcion, emitter);
+        try (OpenShiftClient openShiftClient = createOpenShiftClient(token, servidor, emitter)) {
+            escalarPods(openShiftClient, listaDePodsDown, 0, emitter);
         } catch (Exception e) {
-            System.err.println("Error al intentar escalar los pods hacia abajo: " + e.getMessage());
+            emitter.send(SseEmitter.event().name("error").data("Error al intentar escalar los pods hacia abajo: " + e.getMessage()));
         }
-        System.out.println("Escalado hacia abajo de pods completado.");
+        emitter.send(SseEmitter.event().name("message").data("Escalado hacia abajo de pods completado."));
     }
 
-    public void scaleUpPodsInBlocks(String token, String servidor, String opcion) throws Exception {
-        System.out.println("Iniciando el escalado hacia arriba de los pods en bloques...");
-        List<String> listaDePodsUp = seleccionarListaPods(opcion);
-
-        try (OpenShiftClient openShiftClient = createOpenShiftClient(token, servidor)) {
+    public void scaleUpPodsInBlocks(String token, String servidor, String opcion, SseEmitter emitter) throws IOException {
+        emitter.send(SseEmitter.event().name("message").data("Iniciando el escalado hacia arriba de los pods en bloques..."));
+        List<String> listaDePodsUp = seleccionarListaPods(opcion, emitter);
+        try (OpenShiftClient openShiftClient = createOpenShiftClient(token, servidor, emitter)) {
             for (int i = 0; i < listaDePodsUp.size(); i += BLOCK_SIZE) {
                 List<String> currentBlock = listaDePodsUp.subList(i, Math.min(i + BLOCK_SIZE, listaDePodsUp.size()));
-                System.out.println("Escalando el siguiente bloque de pods: " + currentBlock);
-                escalarPods(openShiftClient, currentBlock, 1);
+                emitter.send(SseEmitter.event().name("message").data("Escalando el siguiente bloque de pods: " + currentBlock));
+                escalarPods(openShiftClient, currentBlock, 1, emitter);
                 boolean allReady;
                 do {
-                    allReady = checkPodsReady(openShiftClient, currentBlock);
+                    allReady = checkPodsReady(openShiftClient, currentBlock, emitter);
                     if (!allReady) {
-                        System.out.println("Esperando a que todos los pods del bloque actual estén listos...");
+                        emitter.send(SseEmitter.event().name("message").data("Esperando a que todos los pods del bloque actual estén listos..."));
                         Thread.sleep(10000); // Espera 10 segundos antes de volver a verificar
                     }
                 } while (!allReady);
-
-                System.out.println(
-                        "Todos los pods del bloque actual están listos. Esperando 2 minutos antes de continuar con el siguiente bloque.");
+                emitter.send(SseEmitter.event().name("message").data("Todos los pods del bloque actual están listos. Esperando 2 minutos antes de continuar con el siguiente bloque."));
                 Thread.sleep(120000); // 120000 milisegundos = 2 minutos
             }
         } catch (Exception e) {
-            System.err.println("Error durante el escalado hacia arriba de los pods: " + e.getMessage());
+            emitter.send(SseEmitter.event().name("error").data("Error durante el escalado hacia arriba de los pods: " + e.getMessage()));
         }
-        System.out.println("Escalado hacia arriba de pods en bloques completado.");
+        emitter.send(SseEmitter.event().name("message").data("Proceso completado"));
     }
 
-    private void escalarPods(OpenShiftClient openShiftClient, List<String> podNames, int replicas) {
-        podNames.forEach(podName -> {
+    private void escalarPods(OpenShiftClient openShiftClient, List<String> podNames, int replicas, SseEmitter emitter) throws IOException {
+        for (String podName : podNames) {
             try {
                 DeploymentConfig dc = openShiftClient.deploymentConfigs()
                         .inNamespace(NAMESPACE)
                         .withName(podName)
                         .get();
                 if (dc != null && dc.getSpec() != null) {
-
                     openShiftClient.deploymentConfigs()
                             .inNamespace(NAMESPACE)
                             .withName(podName)
                             .scale(replicas);
-
-                    System.out.println("Pod " + podName + " escalado a " + replicas + " exitosamente.");
+                    emitter.send(SseEmitter.event().name("message").data("Pod " + podName + " escalado a " + replicas + " exitosamente."));
                 }
             } catch (Exception e) {
-                System.err.println("Error al escalar el pod " + podName + ": " + e.getMessage());
+                emitter.send(SseEmitter.event().name("error").data("Error al escalar el pod " + podName + ": " + e.getMessage()));
+            }
+        }
+        emitter.send(SseEmitter.event().name("message").data("Proceso completado"));
+    }
+
+    private boolean checkPodsReady(OpenShiftClient openShiftClient, List<String> podNames, SseEmitter emitter) throws IOException {
+        return podNames.stream().allMatch(podName -> {
+            try {
+                return openShiftClient.pods()
+                        .inNamespace(NAMESPACE)
+                        .withLabel("name", podName)
+                        .list()
+                        .getItems()
+                        .stream()
+                        .allMatch(pod -> "Running".equals(pod.getStatus().getPhase()));
+            } catch (Exception e) {
+                try {
+                    emitter.send(SseEmitter.event().name("error").data("Error al verificar si los pods están listos: " + e.getMessage()));
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                return false;
             }
         });
     }
 
-    private boolean checkPodsReady(OpenShiftClient openShiftClient, List<String> podNames) {
-        return podNames.stream()
-                .allMatch(podName -> {
-                    try {
-                        return openShiftClient.pods()
-                                .inNamespace(NAMESPACE)
-                                .withLabel("name", podName)
-                                .list()
-                                .getItems()
-                                .stream()
-                                .allMatch(pod -> "Running"
-                                        .equals(pod.getStatus().getPhase()));
-                    } catch (Exception e) {
-                        System.err.println("Error al verificar si los pods están listos: " + e.getMessage());
-                        return false;
-                    }
-                });
-    }
-
-    private List<String> seleccionarListaPods(String opcion) {
+    private List<String> seleccionarListaPods(String opcion, SseEmitter emitter) throws IOException {
         switch (opcion) {
             case "1":
-                System.out.println("Usando lista de Pods opción A.");
+                emitter.send(SseEmitter.event().name("message").data("Usando lista de Pods opción A."));
                 return listaDePodsA;
             case "2":
-                System.out.println("Usando lista de Pods opción B.");
+                emitter.send(SseEmitter.event().name("message").data("Usando lista de Pods opción B."));
                 return listaDePodsB;
             default:
+                emitter.send(SseEmitter.event().name("error").data("Opción no válida proporcionada: " + opcion));
                 throw new IllegalArgumentException("Opción no válida proporcionada: " + opcion);
         }
     }
