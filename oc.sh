51a6592fe0a47fc5061c88eebe2c44d30ec639ed
osgama <134085039+osgama@users.noreply.github.com>
2024-04-27 00:46:53 -0600

    oc token

--- oc.sh
+++ oc.sh
@@ -5,12 +5,23 @@ OC_SERVER="$1"
 USERNAME="$2"
 PASSWORD="$3"
 
-# Login y obtención del token
-oc login $OC_SERVER --username=$USERNAME --password=$PASSWORD --insecure-skip-tls-verify=true
+# Login y redirige la salida estándar y de error a /dev/null
+oc login $OC_SERVER --username=$USERNAME --password=$PASSWORD --insecure-skip-tls-verify=true > /dev/null 2>&1
+
+# Obtiene el token y lo imprime
 TOKEN=$(oc whoami -t)
 
-# Imprime el token
-echo $TOKEN
+# Introduce un retraso para permitir la propagación del token
+sleep 5  # Retraso de 5 segundos
 
-# Cerrar la sesión
-oc logout
+# Verifica la validez del token intentando una operación simple
+oc version --token=$TOKEN > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo $TOKEN  # Solo imprime el token si la verificación es exitosa
+else
+    echo "Token verification failed" >&2  # Imprime error a stderr
+    exit 1
+fi
+
+# Cerrar la sesión, redirigiendo toda la salida a /dev/null
+oc logout > /dev/null 2>&1
--- src/main/java/com/utilidades/servicio/TokenService.java
+++ src/main/java/com/utilidades/servicio/TokenService.java
@@ -9,21 +9,20 @@ import java.io.InputStreamReader;
 public class TokenService {
 
     public String getToken(String server, String username, String password) throws IOException, InterruptedException {
-        String command = String.format("./get-token.sh %s %s %s", server, username, password);
-        Process process = Runtime.getRuntime().exec(command);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        
-        String line;
-        StringBuilder output = new StringBuilder();
-        while ((line = reader.readLine()) != null) {
-            output.append(line + "\n");
+        for (int i = 0; i < 3; i++) {
+            String command = String.format("/app/get-token.sh %s %s %s", server, username, password);
+            Process process = Runtime.getRuntime().exec(command);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String token = reader.readLine();
+            int exitVal = process.waitFor();
+    
+            if (exitVal == 0 && token != null && !token.isEmpty() && !token.contains("failed")) {
+                return token.trim();
+            } else {
+                Thread.sleep(5000);  // Espera 5 segundos antes de intentar de nuevo
+            }
         }
-
-        int exitVal = process.waitFor();
-        if (exitVal == 0) {
-            return output.toString().trim();  // Asume que el script imprime el token directamente
-        } else {
-            throw new RuntimeException("Error in script execution: " + output.toString());
-        }
+        throw new RuntimeException("Failed to obtain a valid token after several attempts.");
     }
+    
 }
