69237654179677f8e8f3add5255f09cdbd60572e
osgama <134085039+osgama@users.noreply.github.com>
2024-08-25 18:32:52 -0600

    mejoras

--- src/main/java/com/utilidades/ps/Application.java
+++ src/main/java/com/utilidades/ps/Application.java
@@ -2,10 +2,9 @@ package com.utilidades.ps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
