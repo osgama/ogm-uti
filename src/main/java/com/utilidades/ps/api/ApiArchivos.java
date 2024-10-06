ed1f74ba568315c33229796e43d25964d31fbb84
osgama <134085039+osgama@users.noreply.github.com>
2024-10-05 22:18:21 -0600

    mejoras v1

--- src/main/java/com/utilidades/ps/api/ApiArchivos.java
+++ src/main/java/com/utilidades/ps/api/ApiArchivos.java
@@ -2,31 +2,77 @@ package com.utilidades.ps.api;
 
 import org.slf4j.*;
 import java.io.*;
-import java.nio.channels.*;
-import java.nio.file.*;
 import java.util.*;
+import java.nio.file.*;
+import java.nio.channels.*;
+import java.text.SimpleDateFormat;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-
-import com.utilidades.ps.servicio.*;
+import org.springframework.web.servlet.mvc.method.annotation.*;
 import net.lingala.zip4j.ZipFile;
-import net.lingala.zip4j.model.ZipParameters;
 import net.lingala.zip4j.model.enums.*;
+import net.lingala.zip4j.model.ZipParameters;
+import com.utilidades.ps.model.*;
+import com.utilidades.ps.servicio.*;
 
 @RestController
-public class ApiDescargaArchivos {
+@RequestMapping("/api")
+public class ApiArchivos {
 
-    private static final Logger logger = LoggerFactory.getLogger(ApiDescargaArchivos.class);
-    private static final int BUFFER_SIZE = 8192;
-
+    private static final Logger logger = LoggerFactory.getLogger(ApiArchivos.class);
     private final DetalleArchivos detalleArchivos;
 
-    public ApiDescargaArchivos(DetalleArchivos detalleArchivos) {
+    public ApiArchivos(DetalleArchivos detalleArchivos) {
         this.detalleArchivos = detalleArchivos;
     }
 
-    @GetMapping("/api/descargar")
+    private static final int BUFFER_SIZE = 16384; // Aumentar el buffer a 16 KB
+
+    @PostMapping("/lista")
+    public ResponseEntity<List<String>> listFiles(@RequestBody ListaArchivosRequest request) {
+
+        String envir = System.getenv("ENVIRONMENT");
+        logger.info(": : : : INICIA BUSQUEDA DE ARCHIVO(S) EN AMBIENTE: {}", envir);
+
+        String directorioFinal = obtenerDirectorioFinal(request.getDirectorio(), request.getTipo());
+        if (directorioFinal == null) {
+            logger.error(": : : : Directorio no válido");
+            return ResponseEntity.badRequest().header("X-Error-Message", "El directorio especificado no es válido.")
+                    .build();
+        }
+
+        File folder = new File(directorioFinal);
+        if (!folder.exists() || !folder.isDirectory()) {
+            logger.error(": : : : El directorio especificado no existe o no es válido: {}", directorioFinal);
+            return ResponseEntity.badRequest()
+                    .header("X-Error-Message", "El directorio especificado no existe o no es válido.").build();
+        }
+
+        File[] files = folder.listFiles();
+        if (files == null || files.length == 0) {
+            logger.info(": : : : No se encontraron archivos en el directorio: {}", directorioFinal);
+            return ResponseEntity.ok(Collections.emptyList());
+        }
+
+        Arrays.sort(files, Comparator.comparingLong(File::lastModified).reversed());
+        List<String> matchingFiles = new ArrayList<>();
+        String filtro = request.getFiltro().toLowerCase();
+        int archivosEncontrados = 0;
+
+        for (File file : files) {
+            if (file.isFile() && (filtro.equals("*") || file.getName().toLowerCase().contains(filtro))) {
+                matchingFiles.add(getFormattedFileDetails(file));
+                archivosEncontrados++;
+            }
+        }
+
+        logger.info(": : : : Archivos encontrados: {}", archivosEncontrados);
+        logger.info(": : : : Filtro: {}", filtro);
+        logger.info(": : : : TERMINA BUSQUEDA DE ARCHIVO(S) EN AMBIENTE: {}", envir);
+        return ResponseEntity.ok(matchingFiles);
+    }
+
+    @GetMapping("/descargar")
     public ResponseEntity<StreamingResponseBody> downloadFile(
             @RequestParam String directorio,
             @RequestParam String archivo,
@@ -69,7 +115,7 @@ public class ApiDescargaArchivos {
         }
     }
 
-    @PostMapping("/api/descargar-zip")
+    @PostMapping("/descargar-zip")
     public ResponseEntity<StreamingResponseBody> downloadFilesAsZip(
             @RequestParam String directorio,
             @RequestParam("archivos") List<String> archivosSeleccionados,
@@ -87,22 +133,30 @@ public class ApiDescargaArchivos {
 
         List<File> archivosParaComprimir = new ArrayList<>();
         for (String nombreArchivo : archivosSeleccionados) {
-            // Elimina comillas adicionales si existen
             nombreArchivo = nombreArchivo.replaceAll("[\"\\[\\]]", "").trim();
             File file = new File(directorioFinal, nombreArchivo);
-            if (file.exists()) {
-                archivosParaComprimir.add(file);
-            } else {
-                logger.error("El archivo no existe: " + file.getAbsolutePath());
+        
+            if (!file.exists()) {
+                logger.error("El archivo no existe: {}", file.getAbsolutePath());
                 return ResponseEntity.notFound().build();
+            } else {
+                logger.info("Archivo encontrado: {}", file.getAbsolutePath());
+                archivosParaComprimir.add(file);
             }
         }
+        
         if (archivosParaComprimir.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
 
         StreamingResponseBody stream = outputStream -> {
-            File tempZip = File.createTempFile("archivos", ".zip");
+            // Genera el nombre del archivo zip, verificando que getFilename() no sea nulo
+            String nombreZip = Optional.ofNullable(generarNombreZip(directorio, tipo, envir).getFilename())
+                                       .orElse("Archivo-Desconocido");
+
+            // Crea el archivo temporal con el nombre generado (sin la extensión .zip)
+            File tempZip = new File("/opt/middleware", nombreZip);
+
             try (ZipFile zipFile = new ZipFile(tempZip)) {
                 ZipParameters parameters = new ZipParameters();
                 parameters.setCompressionMethod(CompressionMethod.DEFLATE);
@@ -119,6 +173,7 @@ public class ApiDescargaArchivos {
                 for (File file : archivosParaComprimir) {
                     zipFile.addFile(file, parameters);
                 }
+
                 // Enviar el ZIP resultante como StreamingResponseBody
                 logger.info(": : : : Archivos Comprimidos y enviados.");
 
@@ -155,6 +210,25 @@ public class ApiDescargaArchivos {
             return System.getenv("BASE_DIRECTORIO_LOGS") + directorio;
         }
         return null;
+    }
+
+    private String getFormattedFileDetails(File file) {
+        String nombre = file.getName();
+        String tamano = getFormattedFileSize(file.length());
+        String fechaModificacion = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss").format(new Date(file.lastModified()));
+        return String.join("||", nombre, tamano, fechaModificacion);
+    }
+
+    private String getFormattedFileSize(long size) {
+        String[] units = { "B", "KB", "MB", "GB", "TB" };
+        int unitIndex = 0;
+        double fileSize = size;
+
+        while (fileSize >= 1024 && unitIndex < units.length - 1) {
+            fileSize /= 1024;
+            unitIndex++;
+        }
+        return "%.2f %s".formatted(fileSize, units[unitIndex]);
     }
 
     private ContentDisposition generarNombreZip(String directorio, String tipo, String envir) {
